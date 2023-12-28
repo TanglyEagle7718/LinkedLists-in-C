b66efef85612d188a662cad64c7e30645ea03cdb
Rishi B <rishi.borra@outlook.com>
2023-12-28 13:04:20 -0500

    Add CLI options

--- linkedList.c
+++ linkedList.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 struct Node {
 	int size;
@@ -15,40 +15,48 @@ void iterateThrough(struct Node *head);
 
 int main() {
 	struct Node *head, *current;
+	
+	int input = -1;
+	int numberOfElements = 0;
+	
+	bool initialized = false;
+	while (input != 3) {
+		if (!initialized)	
+			printf("Options: initialize LinkedList [Type '0'], add to LinkedList [Type '1'], remove from LinkedList [Type '2'], or exit [Type '3'] ");
+		else 	
+			printf("Options: add to LinkedList [Type '1'], remove from LinkedList [Type '2'], or exit [Type '3'] ");
+		scanf("%d", &input);
+		if (input == 0 && !initialized) {
+			initialized = true;	
+			printf("how many elements do you want to add?");
+			scanf("%d", &numberOfElements);
 		
-	printf("Options: initialize LinkedList [Type '0'], add to LinkedList [Type '1'], remove from LinkedList [Type '2']  ");
-	int input;
-	scanf("%d", &input);
-	if (input == 0) {
-	
-		int numberOfElements;
-		printf("how many elements do you want to add?");
-		scanf("%d", &numberOfElements);
-	
-		printf("input data elements in the order you want them in the LinkedList");
-		head = (struct Node *)malloc( sizeof(struct Node) * numberOfElements );
-		current = head;
+			printf("input data elements in the order you want them in the LinkedList");
+			head = (struct Node *)malloc( sizeof(struct Node) * numberOfElements );
+			current = head;
 			
 	
-		getData(head, current, numberOfElements);
-		printList(head);
-	} else if (input == 1) {
-	
-	} else if (input == 2) {
-	
-	}
-	iterateThrough(head);	
+			getData(head, current, numberOfElements);
+			printList(head);
+		} else if (input == 1) {
 
-}
-
-void iterateThrough(struct Node *head) {
+			int index;		
+addInput:
+			printf("what index would you like to add to");
+			scanf("%d", &index);
+			if (head->size < 0) {
+				printf("head is null");
+				head = (struct Node *)malloc( sizeof(struct Node) * 1);
+				head->size = 1
+			} else if (head->size < index) {
+				printf("choose a new index to input to - must be between 0 - %d\n", head->size);
+				goto addInput;
+			}
+		} else if (input == 2) {
 	
-	int index = 0;
-	while (head->next != NULL) {
-		printf("At index INDEX:%d, the data point is DATA:%d and it is located at ADDRESS:%p\n", index++, head->data, head);
-		head = head->next;
+		}
 	}
-	printf("%d\n", head->data);
+
 }
 
 struct Node* getData(struct Node *head, struct Node *current, int numberOfElements) {
